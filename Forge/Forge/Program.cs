4cf9b62e34ccf551f3d6ab31a992472f1db20742
Oliver Nicholls <olivernicholls91@hotmail.com>
2026-06-01 20:19:58 +0100

    Receive input from user.

--- Forge/Forge/Program.cs
+++ Forge/Forge/Program.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.Extensions.Configuration;
-using OpenAI.Chat;
+﻿//using Microsoft.Extensions.Configuration;
+//using OpenAI.Chat;
 
-var builder = new ConfigurationBuilder()
-    .AddUserSecrets<Program>()
-    .Build();
+//var builder = new ConfigurationBuilder()
+//    .AddUserSecrets<Program>()
+//    .Build();
 
-var key = builder["OpenAi:SecretKey"];
+//var key = builder["OpenAi:SecretKey"];
 
-ChatClient client = new("gpt-4.1-mini", key);
+//ChatClient client = new("gpt-4.1-mini", key);
 
-ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
+//ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
 
-Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+//Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+
+
+while (true)
+{
+    // Get the input text. e.g. forge spec --filename myfile.md
+    string? input = Console.ReadLine();
+}
