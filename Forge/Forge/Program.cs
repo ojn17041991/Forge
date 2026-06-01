4fc699b2a8ee924ea1ff624f9ee3632db5261164
Oliver Nicholls <olivernicholls91@hotmail.com>
2026-06-01 20:06:06 +0100

    Port GPT API connection code.

--- Forge/Forge/Program.cs
+++ Forge/Forge/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI.Chat;
+
+var builder = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var key = builder["OpenAi:SecretKey"];
+
+ChatClient client = new("gpt-4.1-mini", key);
+
+ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
+
+Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
